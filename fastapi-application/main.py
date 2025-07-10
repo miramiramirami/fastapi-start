4bfe12661ff18ac201e2c36bfef59731674137b4
miramiramirami <xeless@mail.ru>
2025-07-10 17:56:30 +0500

    first router

--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 import uvicorn
 
+from api import router as api_router
+
 app = FastAPI()
+app.include_router(
+    api_router,
+    prefix='/api'
+)
 
 if __name__ == '__main__':
     uvicorn.run('main:app',reload=True)
