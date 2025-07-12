764bb5cdfdd0e78c204d86d981061732c4eed98d
miramiramirami <xeless@mail.ru>
2025-07-12 13:05:33 +0500

    core.config

--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 import uvicorn
-
+from core.config import settings
 from api import router as api_router
 
 app = FastAPI()
 app.include_router(
     api_router,
-    prefix='/api'
+    prefix=settings.api.prefix,
 )
 
 if __name__ == '__main__':
